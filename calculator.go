782ab4e09d96a9bdfbfa008dcadcbe76553d8cfa
Sirawat Ngarmphandisorn <cardtergt@hotmail.com>
2015-11-18 13:43:26 +0700

    Simple calculation +, -

--- calculator.go
+++ calculator.go
@@ -2,11 +2,21 @@ package main
 
 import "fmt"
 
+func operate(a, b, c int) int {
+	switch c {
+		case 1 : return a + b
+		case 2 : return a - b
+	}
+	return 0
+}
+
 func main() {
-	var first, second int
+	var first, second, case int
 	fmt.Print("First element: ")
 	fmt.Scan(&first)
 	fmt.Print("Second element: ")
 	fmt.Scan(&second)
-	fmt.Println(first + second)
+	fmt.Println("Case 1 : Plus\nCase 2: Minus")
+	fmt.Scan(&case)
+	fmt.Println(operate(first, second, case))
 }
