57ee8e64fa10c237a9a86cebc05d24305bbe5605
Sirawat Ngarmphandisorn <cardtergt@hotmail.com>
2015-11-18 12:58:02 +0700

    Add Question for input

--- calculator.go
+++ calculator.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	var first, second int
+	fmt.Print("First element: ")
 	fmt.Scan(&first)
+	fmt.Print("Second element: ")
 	fmt.Scan(&second)
 	fmt.Println(first + second)
 }
