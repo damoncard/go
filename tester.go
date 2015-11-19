3fe467bf76cccb2ade0f18a6c281d2c1300c99d2
Sirawat Ngarmphandisorn <cardtergt@hotmail.com>
2015-11-19 21:09:36 +0700

    Add more algorithm

--- tester.go
+++ tester.go
@@ -18,6 +18,36 @@ func test_insertSort() {
 	fmt.Println("After sort: ", tools.InsertionSort(arr))
 }
 
+func test_countingSort() {
+	var n int
+	fmt.Print("Insert length of array: ")
+	fmt.Scan(&n)
+	var arr = make([]int, n)
+	fmt.Print("Insert array: ")
+	i := 0
+	for i < n {
+		fmt.Scan(&arr[i])
+		i++
+	}
+	fmt.Println("Before sort: ", arr)
+	fmt.Println("After sort: ", tools.CountingSort(arr))
+}
+
+func test_distributeCounting() {
+	var n int
+	fmt.Print("Insert length of array: ")
+	fmt.Scan(&n)
+	var arr = make([]int, n)
+	fmt.Print("Insert array: ")
+	i := 0
+	for i < n {
+		fmt.Scan(&arr[i])
+		i++
+	}
+	fmt.Println("Before sort: ", arr)
+	fmt.Println("After sort: ", tools.DistributionCounting(arr, tools.Min(arr), tools.Max(arr)))
+}
+
 func test_median() {
 	var n int
 	fmt.Print("Insert length of array: ")
@@ -59,24 +89,16 @@ func test_HornerPolynomial() {
 	fmt.Println("Your result is: ", tools.HornerPo(arr, x))
 }
 
-func test_LefttoRightBiPo() {
-	var base, exponent
-	fmt.Print("Insert base: ")
-	fmt.Scan(&base)
-	fmt.Print("Insert exponent: ")
-	fmt.Scan(&exponent)
-	fmt.Println("Answer is: ", tools.LeftoRightBiPo(base, exponent))
-}
-
 func main() {
 	var s string
 	fmt.Print("What do you want ot test: ")
 	fmt.Scan(&s)
 	switch s {
 		case "insertionsort": test_insertSort()
+		case "countingsort": test_countingSort()
+		case "distributioncounting": test_distributeCounting()
 		case "median": test_median()
 		case "russianmul": test_rusMul()
 		case "hornerpo" : test_HornerPolynomial()
-		case "lefttorightbipo": test_LefttoRightBiPo()
 	}
 }
