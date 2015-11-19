09f5926d05e4b5bfad6b5b74157dec951c8cacc7
Sirawat Ngarmphandisorn <cardtergt@hotmail.com>
2015-11-19 21:17:19 +0700

    Shorten cord (create function getArray)

--- tester.go
+++ tester.go
@@ -3,62 +3,22 @@ package main
 import ("fmt"
 		"./tools");
 
-func test_insertSort() {
-	var n int
-	fmt.Print("Insert length of array: ")
-	fmt.Scan(&n)
-	var arr = make([]int, n)
-	fmt.Print("Insert array: ")
-	i := 0
-	for i < n {
-		fmt.Scan(&arr[i])
-		i++
-	}
+func test_insertSort(arr []int) {
 	fmt.Println("Before sort: ", arr)
 	fmt.Println("After sort: ", tools.InsertionSort(arr))
 }
 
-func test_countingSort() {
-	var n int
-	fmt.Print("Insert length of array: ")
-	fmt.Scan(&n)
-	var arr = make([]int, n)
-	fmt.Print("Insert array: ")
-	i := 0
-	for i < n {
-		fmt.Scan(&arr[i])
-		i++
-	}
+func test_countingSort(arr []int) {
 	fmt.Println("Before sort: ", arr)
 	fmt.Println("After sort: ", tools.CountingSort(arr))
 }
 
-func test_distributeCounting() {
-	var n int
-	fmt.Print("Insert length of array: ")
-	fmt.Scan(&n)
-	var arr = make([]int, n)
-	fmt.Print("Insert array: ")
-	i := 0
-	for i < n {
-		fmt.Scan(&arr[i])
-		i++
-	}
+func test_distributeCounting(arr []int) {
 	fmt.Println("Before sort: ", arr)
 	fmt.Println("After sort: ", tools.DistributionCounting(arr, tools.Min(arr), tools.Max(arr)))
 }
 
-func test_median() {
-	var n int
-	fmt.Print("Insert length of array: ")
-	fmt.Scan(&n)
-	var arr = make([]int, n)
-	fmt.Print("Insert array: ")
-	i := 0
-	for i < n {
-		fmt.Scan(&arr[i])
-		i++
-	}
+func test_median(arr []int) {
 	fmt.Println("Before sort: ", arr)
 	fmt.Println("Median is: ", tools.Median(arr, 0, len(arr)-1))
 }
@@ -89,15 +49,29 @@ func test_HornerPolynomial() {
 	fmt.Println("Your result is: ", tools.HornerPo(arr, x))
 }
 
+func inputArray() []int {
+	var n int
+	fmt.Print("Insert length of array: ")
+	fmt.Scan(&n)
+	var arr = make([]int, n)
+	fmt.Print("Insert array: ")
+	i := 0
+	for i < n {
+		fmt.Scan(&arr[i])
+		i++
+	}
+	return arr
+}
+
 func main() {
 	var s string
 	fmt.Print("What do you want ot test: ")
 	fmt.Scan(&s)
 	switch s {
-		case "insertionsort": test_insertSort()
-		case "countingsort": test_countingSort()
-		case "distributioncounting": test_distributeCounting()
-		case "median": test_median()
+		case "insertionsort": test_insertSort(inputArray())
+		case "countingsort": test_countingSort(inputArray())
+		case "distributioncounting": test_distributeCounting(inputArray())
+		case "median": test_median(inputArray())
 		case "russianmul": test_rusMul()
 		case "hornerpo" : test_HornerPolynomial()
 	}
